cd7ecae2c4c89f3a632dcc1bae8bd1ff0a315d72
kimwereafk <collinskimwere@gmail.com>
2024-04-28 21:05:47 +0300

    initial commit

---
+++ src/components/TransactionsList.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Transaction from "./Transaction";
+
+function TransactionsList({ transactions }) {
+  const list = transactions.map((item) => (
+    <Transaction
+      key={item.id}
+      date={item.date}
+      description={item.description}
+      category={item.category}
+      amount={item.amount}
+    />
+  ));
+
+  return (
+    <table className="ui celled striped padded table">
+      <tbody>
+        <tr>
+          <th>
+            <h3 className="ui center aligned header">Date</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Description</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Category</h3>
+          </th>
+          <th>
+            <h3 className="ui center aligned header">Amount</h3>
+          </th>
+        </tr>
+        {list}
+      </tbody>
+    </table>
+  );
+}
+
+export default TransactionsList;
